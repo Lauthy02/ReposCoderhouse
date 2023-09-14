8ad92ba2fd2c7f9703d005b15e3536544f9dd80f
Lautaro Rojas <lautaro.rojas02@gmail.com>
2023-09-14 11:15:17 -0300

    Correcciones de clase

--- Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/app.js
+++ Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/app.js
@@ -14,9 +14,10 @@ app.get('/api/',(req,res) =>{
 })
 
 app.get('/api/productos',(req,res) =>{ 
-    res.send('aa')
+    res.json({message: 'Todos los usuarios', Usuarios})
 })
 
 app.get('/api/productos/:IdProducto',(req,res) =>{ //localhost:8080/api/productos/2
-    const {IdProducto} = req.params
+    const {IdProducto} = req.params //Todo lo q entra por req.params es string
+    res.json({message: 'Usuario', Usuario})
 })
--- Clases/Clase 06 y clase 07/Servidor express/index.js
+++ Clases/Clase 06 y clase 07/Servidor express/index.js
@@ -28,46 +28,52 @@ const usuarios = [
 //Con esto defino con q ruta va a entrar a este get
 //En el buscador o postman poner: http://localhost:8080/rutadefinidaenlosget
 //req es request y res es response 
-app.get('/',(req,res) =>{ //Esta se llama ruta raíz
+app.get('/', (req, res) => { //Esta se llama ruta raíz
     console.log(req)
-    
+
     res.send('Bienvenido a raíz')
 })
 
-app.get('/primera',(req,res) =>{
+app.get('/primera', (req, res) => {
     res.send('Bienvenido a primera')
 })
 
-app.get('/ultima',(req,res) =>{
+app.get('/ultima', (req, res) => {
     res.send('Bienvenido a última')
 })
 
 //todos los usuarios
-app.get('/usuarios',(req,res) =>{ //Esta se llama ruta raíz
-    res.json({message:'todos los usuarios',usuarios})
+app.get('/raiz', (req, res) => { //Esta se llama ruta raíz
+    res.json({ message: 'todos los usuarios', usuarios })
     //Q pasa si me piden usuarios ordenados o solo 1 usuario, esa info va a estar en params o en query
 })
 
 //1 usuario particular
 //Con los : express sabe q le va a llegar una valor x id, esta variable entra por el req en la propiedad params
-app.get('/usuarios/:id',(req,res) =>{ //Esta se llama ruta raíz
+app.get('/usuarios/:id', (req, res) => { //Esta se llama ruta raíz
     console.log(req.params);
     const idu = req.params //Lo q entra por params siempre es tipo string
-    const usu = usuarios.find(u=>u.id === +idu) //el + es como el parse
-    res.json({message:'Usuario x',usu})
+    const usu = usuarios.find(u => u.id === +idu) //el + es como el parse
+    res.json({ message: 'Usuario x', usu })
 })
 
 //lo q nos puede llegar por query son datos de como aordenar, filtrar, solo 20...
 //Con los ? express sabe q le va a llegar una query
-app.get('/usuarios',(req,res) =>{ 
+//localhost:8080/usuarios?sort=ASC&nombre=Luis&edad=10
+app.get('/usuarios', (req, res) => { //localhost:8080/usuarios o localhost:8080/usuarios?sort=ASC o localhost:8080/usuarios?nombre=Luis
     //Por la query pueden entrar parametros pero si el back no las contembla no va ahacer nada
     console.log(req.query);
-    const {nombre} = req.query //con esto identifioc el parametro
+    const { nombre, sort } = req.query //con esto identifico el parámetro
     if (nombre) {
-        const usu = usuarios.find(u=>nombre === nombre)
+        //localhost:8080/usuarios?nombre=Luis
+        const usu = usuarios.find(u => nombre === nombre)
+        res.json({ message: 'Usuario encontrado', usu })
     }
 
-    res.json({message:'Usuario x',usu})
+    const usuariosFiltrados = sort === 'ASC' ? usuarios.sort((a, b) => a.nombre.localeCompare(b.nombre)) : usuarios.sort((a, b) => b.nombre.localeCompare(a.nombre))
+
+    res.json({ message: 'Usuarios filtrados', usuariosFiltrados })
+
 })
 
 //Más adelante el 8080 se reemplaza por una ruta .com
@@ -84,7 +90,7 @@ app.get('/usuarios',(req,res) =>{
 
 
 //Setear un puerto para que escuche
-app.listen(8080, () =>{
+app.listen(8080, () => {
     console.log('Escuchando el puerto 8080 con express');
 })
 
