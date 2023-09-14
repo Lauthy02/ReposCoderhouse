b17d9c75e3182f296fc9d36186bca165d316714c
Lautaro Rojas <lautaro.rojas02@gmail.com>
2023-09-14 14:33:59 -0300

    Entregable clase 4

--- Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/app.js
+++ Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/app.js
@@ -1,23 +1,93 @@
 //Importo Express
-import express from 'express'
+import express, { query } from 'express'
+//Importo mi clase ProductManager
+import { productManager } from "./class_PorductManager.js";
 
 //Creo el servidor
 const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 //Seteo un puerto para que escuche: localhost:8080/
 app.listen(8080, () => {
     console.log('Escuchando el puerto 8080 con Express');
 })
 
-app.get('/api/',(req,res) =>{ 
+//localhost:8080/api
+app.get('/api/', (req, res) => {
     res.send('Bienvenido a mi servidor con Express')
 })
 
-app.get('/api/productos',(req,res) =>{ 
-    res.json({message: 'Todos los usuarios', Usuarios})
+app.get('/api/productos', async (req, res) => { //Le pongo async al req y al res porque los métodos de mi clas son asíncronos 
+    const {Limite} = req.query
+    try {
+        const TodosLosProductos = await productManager.ObtenerPoductos()
+        if (!TodosLosProductos.length) {
+            res.status(200).json({ message: 'No se encontraron productos' })
+        } else {
+            if (Limite) { //localhost:8080/api/productos?Limite=3
+                const PorductosLimite = []
+                for (let a = 0; a < Limite; a++) {
+                    const Producto = TodosLosProductos[a];
+                    PorductosLimite.push(Producto)
+                }
+                res.status(200).json({ message: 'Prodcutos encontrados con limite', PorductosLimite })
+            } else {
+                res.status(200).json({ message: 'Prodcutos encontrados', TodosLosProductos })
+            }  
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 })
 
-app.get('/api/productos/:IdProducto',(req,res) =>{ //localhost:8080/api/productos/2
-    const {IdProducto} = req.params //Todo lo q entra por req.params es string
-    res.json({message: 'Usuario', Usuario})
+app.get('/api/productos/:IdProducto', async (req, res) => {
+    const { IdProducto } = req.params //Todo lo q entra por req.params es string
+    try {
+        const Producto = await productManager.ObtenerProductoPorID(+IdProducto) //El + es otra forma de parsear el array a int
+        if (!Producto) {
+            res.status(400).json({ message: 'Producto no encontrado con el ID enviado' })
+        } else {
+            res.status(200).json({ message: 'Producto encontrado', Producto })
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+})
+
+app.post('/api/productos', async (req, res) => {
+    try {
+        const NuevoProducto = await productManager.AgregarProducto(req.body)
+        res.status(200).json({ message: 'Poructo creado', NuevoProducto })
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+}) 
+
+app.delete('/api/productos/:IdProducto',async(req,res) => {
+    const { IdProducto } = req.params
+    try {
+        const Respuesta = await productManager.BorrarProducto(+IdProducto)
+        if (Respuesta === -1) {
+            res.status(400).json({message:'Producto no encontrado con el ID enviado'})
+        } else {
+            res.status(200).json({message:'Producto borrado'})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
+})
+
+app.put('/api/productos/:IdProducto',async(req,res) => {
+    const {IdProducto} = req.params
+    try {
+        const Respuesta = await productManager.EditarProducto(+IdProducto,req.body)
+        if (Respuesta === -1) {
+            res.status(400).json({message:'Producto no encontrado con el ID enviado'})
+        } else {
+            res.status(200).json({message:'Producto editado'})
+        }
+    } catch (error) {
+        res.status(500).json({ message: error })
+    }
 })
--- Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/class_PorductManager.js
+++ Clases/Clase 06 y clase 07/DesafioEntregable-Clase4/src/class_PorductManager.js
@@ -1,4 +1,4 @@
-const fs = require('fs')
+import fs from 'fs'
 
 class ProductManager {
 
@@ -8,10 +8,10 @@ class ProductManager {
 
     async ObtenerPoductos() {
         if (fs.existsSync(this.ruta)) {
-            const ProductosGuardados = await fs.promises.readFile(this.ruta,'utf-8')
+            const ProductosGuardados = await fs.promises.readFile(this.ruta, 'utf-8')
             return JSON.parse(ProductosGuardados)
         } else {
-            return [] 
+            return []
         }
     }
 
@@ -21,41 +21,47 @@ class ProductManager {
         if (!ProductosGuardados.length) {
             id = 1
         } else {
-            id = ProductosGuardados[ProductosGuardados.length-1].id+1
+            id = ProductosGuardados[ProductosGuardados.length - 1].id + 1
         }
-        ProductosGuardados.push({id,...producto})
-        await fs.promises.writeFile(this.ruta,JSON.stringify(ProductosGuardados))
-        console.log('Producto cargado')
+        const NuevoProducto = { id, ...producto }
+        ProductosGuardados.push(NuevoProducto)
+        await fs.promises.writeFile(this.ruta, JSON.stringify(ProductosGuardados))
+        return NuevoProducto
     }
 
-    async EditarProducto(IDProducto,CampoAActualizar,NuevoDato){
+    async EditarProducto(IDProducto, objeto) {
         const ProductosGuardados = await this.ObtenerPoductos()
-        const ProductoAActualizar = ProductosGuardados.find(u=>u.id === IDProducto)
-        if (ProductoAActualizar) {
-            ProductoAActualizar[CampoAActualizar] = NuevoDato
-            await fs.promises.writeFile(this.ruta,JSON.stringify(ProductosGuardados))
-        } else {
-          return 'Producto no ecnontrado'  
+        const index = ProductosGuardados.findIndex(u => u.id === IDProducto)
+        if (index === -1) {
+            return -1
         }
+        const Producto = ProductosGuardados[index]
+        ProductosGuardados[index] = {...Producto, ...objeto}
+        await fs.promises.writeFile(this.ruta, JSON.stringify(ProductosGuardados))
+        return 1
     }
 
-    async BorrarProductoPorID(IDProducto) {
+    async BorrarProducto(IDProducto) {
         const ProductosGuardados = await this.ObtenerPoductos()
-        const ProductosGuardadosAux = ProductosGuardados.filter(u=>u.id !== IDProducto)
+        const Producto = ProductosGuardados.find(u=>u.id === IDProducto)
+        if (!Producto) {
+            return -1
+        }
+        const ProductosGuardadosAux = ProductosGuardados.filter(u => u.id !== IDProducto)
         await fs.promises.writeFile(this.ruta, JSON.stringify(ProductosGuardadosAux))
+        return 1
     }
 
     async ObtenerProductoPorID(IDProducto) {
         const ProductosGuardados = await this.ObtenerPoductos()
-        const ProdAux = ProductosGuardados.find(u=>u.id === IDProducto)
-        if (ProdAux) {
-            return ProdAux
-        } else {
-            return 'Porducto no encontrado'
-        }
+        const ProdAux = ProductosGuardados.find(u => u.id === IDProducto)
+        return ProdAux
     }
 
-    async BorrarArchivo(){
+    async BorrarArchivo() {
         await fs.promises.unlink(this.ruta)
     }
 }
+
+//Creo una instancia de ProductManager y la exporto
+export const productManager = new ProductManager('./Productos.json')
